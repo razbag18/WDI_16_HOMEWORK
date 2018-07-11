68a3b45fdc8c14c420e9ae69cf62145230eb4e78
Renae <renaejuvan@gmail.com>
2018-07-11 20:04:04 +1000

    revised loc

--- Renae/wk04/1-mon/loc.rb
+++ Renae/wk04/1-mon/loc.rb
@@ -1,1 +1,11 @@
-IO.readlines('data.txt', '')
+require 'pry'
+
+#CLI command line interface program
+
+print "enter filename: "
+filename = gets.chomp
+
+print IO.readlines('data.txt').length 
+print " lines.\n"
+
+
