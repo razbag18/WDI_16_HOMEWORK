93e48711aafe7ac69076715148cee7840881601e
Renae <renaejuvan@gmail.com>
2018-07-17 18:35:44 +1000

    movies2

--- Renae/wk05/1-mon/movies1/main.rb
+++ Renae/wk05/1-mon/movies1/main.rb
@@ -8,11 +8,14 @@ get '/' do
   erb(:index)
 end
 
+get '/movie_search' do
+  @movie_selection = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&s=' + params[:movie_request])
+  # binding.pry
+  erb(:movie_search)
+end
+
 get '/movie' do
   @movie = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&t=' + params[:movie_request])
   erb(:movie)
 end
-
-
-
 
