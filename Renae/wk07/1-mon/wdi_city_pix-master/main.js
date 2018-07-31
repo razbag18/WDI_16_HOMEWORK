5843bc510233168f9fb66bf3b000e9db0b95efbe
Renae <renaejuvan@gmail.com>
2018-07-31 10:53:30 +1000

    homework

---
+++ Renae/wk07/1-mon/wdi_city_pix-master/main.js
@@ -0,0 +1,28 @@
+
+var cities = ["NYC", "SF", "LA", "SYD", "ATX" ]
+
+cities.forEach(function(city) {
+  var x = document.getElementById('city-type')
+  var option = document.createElement('option')
+  option.text = (city)
+  x.add(option)
+})
+
+document.addEventListener('change', function(event){
+  background = document.querySelector('body')
+  if (event.target.value === "NYC") {
+    background.className = 'nyc'
+  }
+  else if (event.target.value === "SF") {
+    background.className = 'sf'
+  }
+  else if (event.target.value === "LA") {
+    background.className = 'la'
+  }
+  else if (event.target.value === "SYD") {
+    background.className = 'sydney'
+  }
+  else if (event.target.value === "ATX"){
+  background.className = "austin"
+  }
+})
