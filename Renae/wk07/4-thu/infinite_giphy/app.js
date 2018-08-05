145564342abc307c21adafa65875ef3bd055fd11
Renae <renaejuvan@gmail.com>
2018-08-05 19:14:07 +1000

    infinte loop working

--- Renae/wk07/4-thu/infinite_giphy/app.js
+++ Renae/wk07/4-thu/infinite_giphy/app.js
@@ -1,22 +1,23 @@
 var searchForm = $('.search-form');
 var searchBar = $('.search-bar');
 var resultsDiv = $('.results');
+var offset = 10;
 
 function handleSubmit(event) {
   event.preventDefault();
-
+  
   const options = {
     url: `http://api.giphy.com/v1/gifs/search?q=${ (searchBar).val() }&api_key=DeXaS0d74hjF73LTWtIlIslzOGhZGE94&limit=10`,
     method: 'get',
-    dataType: 'json',
-    offset: 10
+    dataType: 'json'
   }
   
   const showSearchResults = function(res){
-    const giphy = res.data;
-
+    resultsDiv.textContent = ""; //not clearing
+    const giphy = res.data; 
+    
     $(giphy).each(function(index, gif){
-      var image = new Image(300, 200)
+      var image = new Image
       var giphyImg = gif.images.downsized.url;
       image.src = giphyImg;
       $(image).append(giphyImg);
@@ -24,7 +25,6 @@ function handleSubmit(event) {
     })
 
   }
-  
   $.ajax(options).done(showSearchResults);
 }
 
@@ -33,7 +33,7 @@ searchForm.on('submit', handleSubmit);
 $(window).scroll(function() {
   if($(window).scrollTop() == $(document).height() - $(window).height()) {
       const options = {
-        url: `http://api.giphy.com/v1/gifs/search?q=${ (searchBar).val() }&api_key=DeXaS0d74hjF73LTWtIlIslzOGhZGE94&limit=10&offset=11`,
+        url: `http://api.giphy.com/v1/gifs/search?q=${ (searchBar).val() }&api_key=DeXaS0d74hjF73LTWtIlIslzOGhZGE94&limit=10&offset=${offset}`,
         method: 'get',
         dataType: 'json'
       }
@@ -48,6 +48,7 @@ $(window).scroll(function() {
           image.src = giphyImg;
           $(image).append(giphyImg);
           $(resultsDiv).append(image);
+          offset ++;
         })
     
       }
@@ -57,29 +58,3 @@ $(window).scroll(function() {
   }
 );
 
-$(window).scroll(function() {
-  if($(window).scrollTop() == $(document).height() - $(window).height()) {
-      const options = {
-        url: `http://api.giphy.com/v1/gifs/search?q=${ (searchBar).val() }&api_key=DeXaS0d74hjF73LTWtIlIslzOGhZGE94&limit=10&offset=21`,
-        method: 'get',
-        dataType: 'json'
-      }
-      
-      const showSearchResults = function(res){
-        
-        const giphy = res.data;
-    
-        $(giphy).each(function(index, gif){
-          var image = new Image(300, 200)
-          var giphyImg = gif.images.downsized.url;
-          image.src = giphyImg;
-          $(image).append(giphyImg);
-          $(resultsDiv).append(image);
-        })
-    
-      }
-    
-      $.ajax(options).done(showSearchResults);
-    }
-  }
-);
